d329c1e9e08fbfe63465db9edf69b39e9f77e6ad
Alvise Spanò <spano.alvise@gmail.com>
2021-12-13 14:04:14 +0100

    Update Lesson17.fs

--- src/Lesson17.fs
+++ src/Lesson17.fs
@@ -6,6 +6,7 @@ type ty =
     | TyName of string
     | TyArrow of ty * ty
     | TyVar of tyvar
+    | TyTuple of ty list
 
 type scheme = Forall of tyvar list * ty
 
@@ -20,7 +21,7 @@ type expr =
     | App of expr * expr
     | Var of string
     | Let of string * expr * expr
-    | LetRec of string * string * expr * expr
+    | LetRec of string * ty * expr * expr
     | IfThenElse of expr * expr * expr option
     | Tuple of expr list
     
@@ -65,8 +66,10 @@ let rec pretty_expr e =
     | Let (x, e1, e2) ->
         sprintf "let %s = %s in %s" x (pretty_expr e1) (pretty_expr e2)
 
-    | LetRec (f, x, e1, e2) ->
-        sprintf "let rec %s %s = %s in %s" f x (pretty_expr e1) (pretty_expr e2)
+    | LetRec (f, tf, e1, e2) ->
+        sprintf "let rec %s : %s = %s in %s"
+            f (pretty_ty tf)
+            (pretty_expr e1) (pretty_expr e2)
 
     | IfThenElse (e1, e2, e3o) ->
         let s = sprintf "if %s then %s" (pretty_expr e1) (pretty_expr e2)
@@ -82,7 +85,9 @@ let rec pretty_expr e =
             | e :: es -> sprintf "%s, %s" (pretty_expr e) (f es)
         sprintf "(%s)" (f es)
 
-let rec typecheck_expr (env : ty env) e =
+let type_error msg = sprintf "type error: %s" msg
+
+let rec typecheck_expr (env : ty env) (e : expr) : ty =
     match e with
     | Lit (LInt _) -> TyName "int"
     | Lit (LFloat n) -> TyName "float"
@@ -94,3 +99,36 @@ let rec typecheck_expr (env : ty env) e =
     | Lambda (x, Some t1, e) ->
         let t2 = typecheck_expr ((x, t1) :: env) e
         TyArrow (t1, t2)
+
+    | App (e1, e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        match t1 with
+        | TyArrow (l, r) ->
+            if l = t2 then r 
+            else type_error (sprintf "wrong application: %s does not match %s" (pretty_ty t2) (pretty_ty l)) 
+        | _ -> type_error (sprintf "expecting an arrow on left side of application but got %s" (pretty_ty t1))
+
+    | Let (x, e1, e2) ->
+        let t1 = typecheck_expr env e1
+        typecheck_expr ((x, t1) :: env) e2
+
+    | IfThenElse (e1, e2, Some e3) ->
+        let t1 = typecheck_expr env e1
+        if t1 <> TyName "bool" then type_error (sprintf "if condition must be a bool but got a %s" (pretty_ty t1))
+        let t2 = typecheck_expr env e2
+        let t3 = typecheck_expr env e3
+        if t2 <> t3 then type_error (sprintf "type mismatch in then (%s) and else (%s)" (pretty_ty t2) (pretty_ty t3))
+        t2
+
+    | Tuple es ->
+        TyTuple (List.map (typecheck_expr env) es)
+        
+    | LetRec (f, tf, e1, e2) ->
+        let env0 = (f, tf) :: env
+        let t1 = typecheck_expr env1 e1
+        match t1 with
+        | TyArrow _ -> ()
+        | _ -> failwithf "typecheck_expr: letrec is restricted to functions but got type %s" (pretty_ty t1)
+        if t1 <> tf then failwithf "typecheck_expr: letrec type mismatch: expected %s but got %s" (pretty_ty tf) (pretty_ty t1)
+        typecheck_expr env0 e2
