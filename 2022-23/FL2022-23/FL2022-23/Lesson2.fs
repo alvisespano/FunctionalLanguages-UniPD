1746a730356116ce50649249ddfd0c26e3cf83b5
Alvise Spanò <spano.alvise@gmail.com>
2022-10-26 10:23:42 +0200

    2022-23 first push

---
+++ 2022-23/FL2022-23/FL2022-23/Lesson2.fs
@@ -0,0 +1,98 @@
+﻿module Lesson2
+
+
+let k = 4
+
+let f = fun x -> x + k
+
+let g (x, y, z) = (x + 1, y + z)
+
+let g2 = fun (x, y, z) -> (x + 1, y + z)
+
+///////////////////////////////
+
+let rec size_of_list l =
+    match l with
+    | [] -> 0
+    | h :: t -> 1 + size_of_list t
+
+
+type color = Red | Black | Yellow | Rgb of int * int * int
+
+let col1 = Black
+
+let pretty_print_color c =
+    match c with
+    | Red -> "red"
+    | Black -> "black"
+    | Yellow -> "yellow"
+    | Rgb (r, g, b) -> sprintf "[R:%d G:%d B%d]" r g b
+
+let str1 = pretty_print_color Red
+
+
+
+type 'a list = [] | (::) of 'a * 'a list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    (*
+enum Color { RED, YELLOW, BLACK, WHITE, BLUE };
+
+int n = 3;
+Color col1 = RED;
+
+switch(col1) {
+case RED: ...
+case YELLOW: ..
+default: ...
+}
+
+
+switch(n) {
+case 0: do something; break;
+case 1: ...
+default: ...
+}
+
+*)
+
+(*struct list_node {
+    int data;
+    struct list_node* next;
+};
+
+unsigned int size_of_list(struct list_node* n) {
+    if (n == NULL)
+        return 0;
+    return size_of_list(n->next) + 1;
+}*)
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2022-23/FL2022-23/FL2022-23/Main.fs
@@ -0,0 +1,54 @@
+﻿
+
+
+let k = 4
+
+let f x = x + 1
+
+let g (x, y) = x + 1 + y
+
+let h (x, y, z) = if x && y > 2 then y else z
+
+let j (x, y) = y (x + 1) + 1
+
+let u (a, b, c) = if a (b c) then c else 3
+
+let i x = x
+
+let p1 = (i 4, i true, i (if i 5 > 6 then i 4 else i (i 3)))
+
+let swap (x, y) = (y, x)
+
+// swap2: 'a * 'a * ('a -> 'b) -> 'b * 'b
+let swap2 (x, y, f) = (f y, f x)
+
+
+let app1 (f, x) = f (x + 1)
+
+
+
+
+
+
+//ArrayList<Integer> l = new ArrayList<Integer>();
+
+let l0 = [ (fun x -> x + 1); (fun x -> x * 2)]
+
+
+let l1 = [1; 5; 9; 10 + 3; 89]
+
+let l2 = [true; false; true && false || true]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
