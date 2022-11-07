3bf19f0d07b7422b5bcb9ccca25cbc8e4eeff203
Alvise Spanò <spano.alvise@gmail.com>
2022-11-07 14:02:12 +0100

    Update Lesson3.fs

--- 2022-23/Lessons/Lesson3.fs
+++ 2022-23/Lessons/Lesson3.fs
@@ -29,15 +29,16 @@ let rec length_of_list l =
     | [] -> 0                               // [] represents the base case of the recursion
     | x :: xs -> 1 + length_of_list xs      // x and xs are free variable names: they are bound to the RIGHT and the LEFT arguments of (::), which are the head and the tail of the list
 
+
 // lets redefine the list type without shadowing the original definition in the F# standard library
 type 'a list = Empty                        // empty list case
-             | NonEmpty of 'a * 'a list     // non-empty list case: rather than an infix operator we use a normal name attacched to a paiur
+             | Cons of 'a * 'a list     // non-empty list case: rather than an infix operator we use a normal name attacched to a paiur
 
 // the function lenght_of_list above would become like this
 let rec length_of_list2 l =
     match l with
     | Empty -> 0                                    // Empty represents the base case of the recursion
-    | NonEmpty (x, xs) -> 1 + length_of_list2 xs    // x and xs are bound to the 2 arguments of NonEmpty: x is the head and xs is the tail
+    | Cons (x, xs) -> 1 + length_of_list2 xs    // x and xs are bound to the 2 arguments of NonEmpty: x is the head and xs is the tail
 
 // in C that would be like:
 (*
@@ -66,6 +67,9 @@ type color = Red | Black | Yellow | Rgb of int * int * int
 
 let col1 = Black    // col1 : color
 
+let violet = Rgb (255, 0, 255)
+
+
 // pretty print a color
 let pretty_print_color c =  // pretty_print_color : color -> string
     match c with
@@ -76,6 +80,39 @@ let pretty_print_color c =  // pretty_print_color : color -> string
 
 // str1 : string
 let str1 = pretty_print_color Red
+
+
+module Tree =
+
+    type tree = Leaf of int | Node of tree * tree
+
+    let rec sum_tree t =
+        match t with
+        | Leaf n -> n
+
+        | Node (t1, t2) -> sum_tree t1 + sum_tree t2
+
+
+    let tree1 = Leaf 56
+
+    let tree2 = Node (tree1, tree1)
+
+    let tree3 = Node (Node (Node (Leaf 1, Leaf 2), Leaf 3), Leaf 4)
+
+
+
+module MyList =
+
+
+    type 'a mylist = Empty | Cons of 'a * 'a mylist 
+
+    let ll1 = Empty
+    let ll2 = Cons ("ciao", Cons ("sono", Cons (3, Empty)))
+    let ll3 = Cons (true, Cons (true, Cons (false, Empty)))
+
+
+
+
 
 
 // in C the same would be an enumeration, but enumerations do not support data attached to labels
