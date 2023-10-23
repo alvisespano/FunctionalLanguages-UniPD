7922be19b837114d28f85ab3660201dd46d735aa
Alvise Spanò <spano.alvise@gmail.com>
2023-10-23 14:08:35 +0200

    Currying

---
+++ 2023-24/Lessons23_24/basics.fs
@@ -0,0 +1,112 @@
+﻿module Main
+
+
+let x = 7
+
+let y = "ciao"
+
+let (a, b) = (1, true)
+
+let f x = x + 1 // f : int -> int
+
+let g x = if x then 1 else 2  // g : bool -> int
+
+
+(*
+IMPERATIVE
+----------
+int i = 0;
+while (i < 10) {
+    i = i + 1;      // assigment = imperative
+}
+
+RECURSIVE
+---------
+void recfun(int i) {
+    if (i < 10) recfun(i + 1);  // recursion does not modify anything
+}
+*)
+
+let rec recfun i = 
+    if i < 10 then
+        recfun (i + 1)
+
+(* 
+int fib(int x) {
+    if (x < 2) return 1;
+    else return fib(x - 1) + fib(x - 2);
+}
+*)
+
+let rec fib x =
+    if x < 2 then 1
+    else fib (x - 2) + fib (x - 1)
+
+(*
+int fact(int x) {
+    return x < 2 ? 1 : x * fact(x - 1);
+}
+*)
+
+let rec fact x = 
+    if x < 2 then 1 else x * fact(x - 1)
+
+
+//////////////////////////////
+
+let swap (a, b) = (b, a)
+
+let (p, q) = swap (1, 56)
+let (r, t) = swap (true, "ciao")
+let (v, w) = swap ((4.5, "ciccio"), 7)
+
+(*
+public class Pair<A, B> {
+    public A first;
+    public B second;
+}
+
+public class Swap {
+    public static <A, B> Pair<B, A> swap(Pair<A, B> p) {
+        return new Pair(p.second, p.first);
+    }
+}
+*)
+
+
+(*
+JAVA
+----
+public static Object ident(Object x) { return x; }
+public static <T> T ident(T x) { return x; }
+
+public static <A extends String, B extends Number> A f(A a, B b) { ..... }
+-----> String f(String a, Number b)
+...
+
+Object a = ident(new ArrayList<Integer>());
+
+
+C
+-
+int ident_int(int x) { return x; }
+char* ident_charpt(char* x) { return x; }
+void* ident_voidpt(void* x) { return x; }
+*)
+
+let ident x = x
+
+let ident2 = fun x -> x
+
+// ident(x) = x
+
+
+
+let swap2 (x, y) = (y, x)
+
+
+
+fun x -> fun y -> x * 2 + y
+
+
+
---
+++ 2023-24/Lessons23_24/currying.fs
@@ -0,0 +1,47 @@
+﻿module currying
+
+
+
+// curry : ('a -> 'b -> 'c) -> 'a * 'b -> 'c
+let curry f (x, y) = f x y
+
+// uncurry : ('a * 'b -> 'c) -> 'a -> 'b -> 'c
+let uncurry f x y = f (x, y)
+
+////////////////////////
+
+// addition : int -> (int -> int)
+let addition = fun x -> fun y -> x + y
+
+// addition2 : int * int -> int
+let addition2 = fun (x, y) -> x + y
+
+let z = addition 7 // z : int -> int
+
+let z2 = addition2 (7, 8)
+
+let eight = z 1
+
+/////////////////////////
+
+// pow : int -> _ -> _
+let rec pow bas exp =
+    if exp > 1 then bas * (pow bas (exp - 1))
+    else 1
+
+let rec pow2 (bas, exp) = 
+    if exp > 1 then bas * (pow2 (bas, exp - 1))
+    else 1
+
+
+
+
+
+
+
+
+
+
+
+
+
