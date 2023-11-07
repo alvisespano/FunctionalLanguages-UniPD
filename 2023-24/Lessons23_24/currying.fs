2e66e0c8a8ae0c6e15ca2b02aa6b2ee6f520d461
Alvise Spanò <spano.alvise@gmail.com>
2023-11-07 14:06:18 +0100

    Lesson 7 11 23

--- 2023-24/Lessons23_24/currying.fs
+++ 2023-24/Lessons23_24/currying.fs
@@ -24,14 +24,66 @@ let eight = z 1
 
 /////////////////////////
 
-// pow : int -> _ -> _
+let x = 9
+
+let rec f x =
+    let f x = x + 1
+    let rec f x = f (x + 1)
+    f x
+
+let rec r () = r () + 1
+
+// pow : int -> int -> int
 let rec pow bas exp =
     if exp > 1 then bas * (pow bas (exp - 1))
     else 1
 
+// pow2 : int * int -> int
 let rec pow2 (bas, exp) = 
     if exp > 1 then bas * (pow2 (bas, exp - 1))
     else 1
+
+
+////////////////////////////
+
+(*public class Animal {
+    protected int weight;
+    
+    public Animal(int w) { this.weight = w; }
+
+    public void eat(Animal a) { this.weight += a.weight; }
+}
+
+public class Dog extends Animal {
+    public Dog(int w) { super(w); }
+    @Override
+    public void eat(Animal a) { this.eat(this); } 
+}
+
+public static void main() {
+    Animal poppy = new Dog(10);
+    Dog toby = new Dog(20);
+    poppy.eat(toby);
+}*)
+
+
+(*
+struct S {
+   int a;
+   double b;
+   char** c;
+}
+
+union U {
+   int n;
+   char bytes[4];
+}
+
+enum Color { BLACK, RED, WHITE, YELLOW };
+*)
+
+
+
 
 
 
---
+++ 2023-24/Lessons23_24/unions.fs
@@ -0,0 +1,59 @@
+﻿module unions
+
+
+
+type baudo = int
+
+
+type person = { name : string; surname : string; age : int }
+
+
+type color = Black | Yellow | White | Red | Rgb of int * int * int
+
+let col1 = Rgb
+
+let make_brigher c =
+    match c with
+    | Black  -> White
+    | Red    -> Yellow
+    | White  -> White
+    | Yellow -> Yellow
+    | Rgb (r, g, b) -> Rgb (r * 2, g * 2, b * 2)
+
+
+
+type mylist_ugly = { data : int; next : mylist }
+
+let l1 = { data = 1; next = { data = 2 ; next = { data = 3 ; next = }}}
+
+
+type mylist = Empty | NonEmpty of int * mylist
+
+let l2 = Empty
+
+let l3 = NonEmpty (1, NonEmpty (2, Empty))
+
+
+
+
+//type 'a list = [] | (::) of 'a * 'a list
+
+let l4 = 1 :: 45 :: 765 :: 3 :: []
+
+let l5 = [1; 45; 765; 3]
+
+let l6 = []
+
+let l7 = ["ciao"; "sono"; "io"]
+
+let l8 = [(1, true); (5, false)]
+
+let l9 = [[1]; []; [1; 2; 3]]
+
+let l10 = ([[[1]]], [fun x -> x; fun x -> x + 1])
+
+
+
+
+
+
