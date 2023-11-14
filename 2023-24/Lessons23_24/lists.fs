09b4f5e92335562484b2cdb29061aceb3ba49114
Alvise Spanò <spano.alvise@gmail.com>
2023-11-14 14:06:13 +0100

    Folds and trees

--- 2023-24/Lessons23_24/lists.fs
+++ 2023-24/Lessons23_24/lists.fs
@@ -25,3 +25,61 @@ let rec filter p l =
     | h :: t -> let u = filter p t
                 if p h then h :: u else u
                 
+// iter : ('a -> unit) -> 'a list -> unit
+let rec iter f l =
+    match l with
+    | [] -> ()
+    | h :: t -> f h; iter f t
+
+// sum_mono : int list -> int
+let rec sum_mono l =
+    match l with
+    | [] -> 0
+    | h :: t -> h + sum_mono t
+
+// sum : ('a -> 'b -> 'b) -> 'b -> 'a list -> 'b
+let rec sum (+) zero l =
+    match l with
+    | [] -> zero
+    | h :: t -> h + sum (+) zero t
+
+// fold_back : ('a -> 'b -> 'b) -> 'b -> 'a list -> 'b
+let rec fold_back f accu l =
+    match l with
+    | [] -> accu
+    | h :: t -> f h (fold_back f accu t)
+
+// fold : ('a -> 'b -> 'b) -> 'b -> 'a list -> 'b
+let rec fold f accu l =
+    match l with
+    | [] -> accu
+    | h :: t -> fold f (f h accu) t
+
+let filter_by_fold_back p l = 
+    fold_back (fun x accu -> if p x then x :: accu else accu ) [] l
+
+let filter_by_fold p l = 
+    fold (fun x accu -> if p x then accu @ [x] else accu) [] l
+
+let map_by_fold f l =
+    fold (fun x accu -> accu @ [f x]) [] l
+
+
+
+
+(*
+public static <A, B> B fold(BiFunction<A, B, B> f, B accu, Collection<A> c) {
+    for (A x : c) {
+        accu = f.apply(x, accu);
+    }
+    return accu;
+}
+
+public static <A, B> B recfold(BiFunction<A, B, B> f, B accu, List<A> c) {
+    if (c.isEmpty()) return accu;
+    A h = c.get(0)
+    c.removeAt(0);
+    return f.apply(h, recfold(f, accu, c));
+}
+*)
+
---
+++ 2023-24/Lessons23_24/trees.fs
@@ -0,0 +1,13 @@
+﻿module trees
+
+type 'a bintree = Leaf | Node of 'a * 'a bintree * 'a bintree
+
+let tree1 = Node (1, Node (2, Node (3, Leaf, Leaf), Leaf), Node (5, Leaf, Leaf))
+
+let rec print_bintree t =
+    match t with
+    | Leaf -> ()
+    | Node (data, left, right) ->
+        printf "%O\n" data;
+        print_bintree left;
+        print_bintree right
